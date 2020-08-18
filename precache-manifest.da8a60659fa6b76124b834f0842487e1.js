08c0b48c01effb51c2aaa284722fff4fc9e4260f
Jorge <jym990@gmail.com>
2020-08-17 19:40:06 -0500

    Updates

---
+++ precache-manifest.da8a60659fa6b76124b834f0842487e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19bcf77abec0ee47d55945e22f923065",
+    "url": "/chatroom-chatbot/index.html"
+  },
+  {
+    "revision": "3eeb058331822a6698fb",
+    "url": "/chatroom-chatbot/static/css/main.670ccef9.chunk.css"
+  },
+  {
+    "revision": "b99a75e860d251d72542",
+    "url": "/chatroom-chatbot/static/js/2.018c0b1a.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chatroom-chatbot/static/js/2.018c0b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eeb058331822a6698fb",
+    "url": "/chatroom-chatbot/static/js/main.470e803c.chunk.js"
+  },
+  {
+    "revision": "345cb8ba7422553604a1",
+    "url": "/chatroom-chatbot/static/js/runtime-main.372bc08a.js"
+  }
+]);
